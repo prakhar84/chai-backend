7173a76b43fe8efc053e2fee6a6a253624e9256e
Prakhar Tamrakar <prakhar.tamrakar@symbtechnologies.com>
2025-06-30 22:26:21 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -5,7 +5,18 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({ path: "./.env" });
-connectDB();
+connectDB()
+    .then(() => {
+        app.on("error", (err) => {
+            console.log("Error :", err);
+        });
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running at port: ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("MONGODB connection failed !!!: ", err);
+    });
 
 /* approach 2
 import express from "express";
